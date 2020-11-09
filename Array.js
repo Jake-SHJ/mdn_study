a460dae46d620e649fff4ae3dca4b86f9ee5466f
jake <guswns1853@gmail.com>
2020-11-09 23:48:53 +0900

    Array 자주 사용하는 연산 작성 시작

---
+++ Array.js
@@ -0,0 +1,18 @@
+/**
+ * 자주 사용하는 연산
+ */
+
+// 배열 만들기
+
+let fruits = ["apple", "banana"];
+console.log(fruits.length); // 2
+console.log("-----");
+
+// 인덱스로 배열의 항목에 접근하기
+
+let first = fruits[0];
+console.log(first); // apple
+
+let last = fruits[fruits.length - 1];
+// 총 배열의 길이 - 1은 무조건 마지막 배열의 요소
+console.log(last); // banana
