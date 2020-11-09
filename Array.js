5ffa561f75467bae72b3b673c0a545b55c03a731
jake <guswns1853@gmail.com>
2020-11-09 23:54:05 +0900

    배열 순회 처리, 배열 끝 항목 추가

--- Array.js
+++ Array.js
@@ -13,6 +13,20 @@ console.log("-----");
 let first = fruits[0];
 console.log(first); // apple
 
-let last = fruits[fruits.length - 1];
-// 총 배열의 길이 - 1은 무조건 마지막 배열의 요소
+let last = fruits[fruits.length - 1]; // 총 배열의 길이 - 1은 무조건 마지막 배열의 요소
 console.log(last); // banana
+console.log("-----");
+
+// 배열의 항목들을 순회하며 처리하기
+
+fruits.forEach(function (item, index, array) {
+  console.log(item, index, array);
+});
+// apple 0 [ 'apple', 'banana' ]
+// banana 1 [ 'apple', 'banana' ]
+console.log("-----");
+
+// 배열 끝에 항목 추가하기
+let newLength = fruits.push("orange");
+console.log(fruits); // [ 'apple', 'banana', 'orange' ]
+console.log("-----");
